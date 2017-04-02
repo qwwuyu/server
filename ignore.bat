5c8d493410ad3c67bc1d9e462d634abaacc53b87
qiwei <z4040409@163.com>
2017-04-02 20:59:46 +0800

    隐藏文件

---
+++ ignore.bat
@@ -0,0 +1,3 @@
+git update-index --assume-unchanged mybatis\generatorConfig.xml
+git update-index --assume-unchanged src\main\resources\jdbc.properties
+git update-index --assume-unchanged .classpath
---
+++ show.bat
@@ -0,0 +1,3 @@
+git update-index --no-assume-unchanged mybatis\generatorConfig.xml
+git update-index --no-assume-unchanged src\main\resources\jdbc.properties
+git update-index --no-assume-unchanged .classpath
