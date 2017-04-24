348ddf037adee0fb7ab30359ec23062cf9f70914
wei.qi <wei.qi@yg-ai.com>
2017-04-24 20:02:30 +0800

    3

--- src/main/java/com/qwwuyu/server/utils/CommUtil.java
+++ src/main/java/com/qwwuyu/server/utils/CommUtil.java
@@ -1,5 +1,9 @@
 package com.qwwuyu.server.utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class CommUtil {
 	public static boolean isWindows() {
 		return SystemUtils.IS_OS_WINDOWS;
@@ -8,7 +12,18 @@ public class CommUtil {
 	public static void sleep(long millis) {
 		try {
 			Thread.sleep(millis);
-		} catch (InterruptedException e) {
+		} catch (InterruptedException e) {}
+	}
+
+	public static String read(String name) throws Exception {
+		String path = CommUtil.class.getClassLoader().getResource(name).getPath();
+		BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+		StringBuilder builder = new StringBuilder();
+		String read = null;
+		while ((read = reader.readLine()) != null) {
+			builder.append(read).append("\n");
 		}
+		reader.close();
+		return builder.substring(0, builder.length() - 1).toString();
 	}
 }
