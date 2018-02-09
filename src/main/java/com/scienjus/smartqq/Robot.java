b49834ba791091ee7854628d4859288f631cee3a
qwwuyu <qiweiwuyu@gmail.com>
2018-02-09 16:38:48 +0800

    修改QQ机器人

--- src/main/java/com/scienjus/smartqq/Robot.java
+++ src/main/java/com/scienjus/smartqq/Robot.java
@@ -60,84 +60,82 @@ public class Robot {
 
 		@Override
 		public void onMessage(Message message) {
-			lastTime = System.currentTimeMillis();
-			String content = message.getContent();
-			if (content.startsWith(pwd)) {
-				admin = message.getUserId();
-				nike = "@" + content.substring(pwd.length());
-				executor.execute(() -> client.sendMessageToFriend(admin, "收到id" + nike));
-			}
+			onMessage(message.getContent(), message.getUserId(), message.getUserId(), (msg, chatId) -> client.sendMessageToFriend(chatId, msg));
+		}
+
+		@Override
+		public void onGroupMessage(GroupMessage message) {
+			onMessage(message.getContent(), message.getUserId(), message.getGroupId(), (msg, chatId) -> client.sendMessageToGroup(chatId, msg));
+		}
+
+		@Override
+		public void onDiscussMessage(DiscussMessage message) {
+			onMessage(message.getContent(), message.getUserId(), message.getDiscussId(), (msg, chatId) -> client.sendMessageToDiscuss(chatId, msg));
 		}
 
 		private String lastMsg = null;
 
-		@Override
-		public void onGroupMessage(GroupMessage message) {
-			String content = message.getContent();
-			if (content.startsWith(pwd)) {
-				admin = message.getUserId();
-				nike = "@" + content.substring(pwd.length());
-				executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "get" + nike));
-				return;
-			}
-			if (admin == 0L || message.getContent().equals(lastMsg)) {
-				return;
-			}
+		private void onMessage(String content, long userId, long chatId, ISendMessage send) {
 			lastTime = System.currentTimeMillis();
-			lastMsg = message.getContent();
-			long groupId = message.getGroupId();
-			Long robot = robotMap.get(groupId);
-			if (robot == null) robot = 0L;
-			if (0L == robot && content.startsWith(nike)) {
-				robot = message.getUserId();
-				robotMap.put(groupId, robot);
-				nikeMap.put(groupId, nike);
-				flagMap.put(groupId, 0);
-				executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "get"));
-			} else if (admin == message.getUserId()) {
+			if (content.startsWith(pwd)) {
+				admin = userId;
+				nike = "@" + content.substring(pwd.length());
+				send(send, "收到id" + nike, chatId);
+			}
+			if (admin == 0L || content.equals(lastMsg)) {
+				return;
+			}
+			lastMsg = content;
+			Long robot = robotMap.get(chatId);
+			if (robot == null && content.startsWith(nike)) {
+				robot = userId;
+				robotMap.put(chatId, robot);
+				nikeMap.put(chatId, nike);
+				flagMap.put(chatId, 0);
+				send(send, "get", chatId);
+			} else if (admin == userId) {
 				if ("重新获取".equals(content)) {
-					robotMap.put(groupId, null);
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "重新获取id"));
+					robotMap.remove(chatId);
+					send(send, "重新获取id", chatId);
 				} else if (content.startsWith(tag + "开始-")) {
 					// 开始-钓鱼-大腿.+[积分|鱼饵][\D]+(\d+)分钟-探险-大腿.+[宠物|探险][\D]+(\d+)分钟
-					// 开始-进入副本 神陨星域-asd.+[副本|荒古][\D]+(\d+)分钟-双修-asd.+[双修|活动][\D]+(\d+)分钟
-					if (!waitMap.containsKey(groupId)) {
+					if (!waitMap.containsKey(chatId)) {
 						final ArrayList<String[]> msgs = new ArrayList<>();
 						String[] command = content.substring((tag + "开始-").length()).split("-");
 						if (command.length % 3 != 0) {
-							executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "指令格式错误"));
+							send(send, "指令格式错误", chatId);
 							return;
 						}
 						for (int i = 0; i < command.length; i += 3) {
 							msgs.add(new String[] { command[i], command[i + 1], command[i + 2] });
 						}
-						msgsMap.put(groupId, msgs);
-						final Wait wait = new Wait(new MyWaitCallBack(client, groupId));
-						waitMap.put(groupId, wait);
+						msgsMap.put(chatId, msgs);
+						final Wait wait = new Wait(new MyWaitCallBack(client, chatId));
+						waitMap.put(chatId, wait);
 						executor.execute(() -> {
 							try {
-								client.sendMessageToGroup(message.getGroupId(), "start");
+								send(send, "start", chatId);
 								for (int i = 0; i < msgs.size(); i++) {
 									CommUtil.sleep(1000);
 									int t = Math.max(1, Integer.parseInt(msgs.get(i)[2]));
-									wait.addTag(msgs.get(i)[0], t * 60 * 1000);
+									wait.addTag(msgs.get(i)[0], t * 1000);
 								}
 							} catch (Exception e) {}
 						});
 					} else {
-						executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "挂机过了"));
+						send(send, "挂机过了", chatId);
 					}
 				} else if ((tag + "暂停").equals(content)) {
-					Wait wait = waitMap.remove(groupId);
-					ArrayList<String[]> remove = msgsMap.remove(groupId);
+					Wait wait = waitMap.remove(chatId);
+					ArrayList<String[]> remove = msgsMap.remove(chatId);
 					if (remove != null) {
 						remove.clear();
 					}
 					if (wait != null) {
 						wait.stop();
-						executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "stop"));
+						send(send, "stop", chatId);
 					} else {
-						executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "你没有开始"));
+						send(send, "你没有开始", chatId);
 					}
 				} else if ((tag + "停止").equals(content)) {
 					for (Wait wait : waitMap.values()) {
@@ -145,52 +143,33 @@ public class Robot {
 					}
 					waitMap.clear();
 					executor.execute(() -> {
-						client.sendMessageToGroup(message.getGroupId(), "end");
+						send.sendMessage("end", chatId);
 						closeSmartQQ(tag);
 					});
 				} else if (content.startsWith(tag + "-")) {
 					final String txt = content.substring((tag + "-").length());
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), txt));
+					send(send, txt, chatId);
 				}
 			}
-			if (robot == message.getUserId()) {
-				final Integer flag = flagMap.get(groupId);
-				String nike = nikeMap.get(groupId);
-				if (content.contains(nike + " 您的宠物已经精疲力竭了") && flag < 2) {
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), flag == 0 ? "使用 小精力瓶" : "使用 大精力瓶", "进入副本 神陨星域"));
-				} else if (content.contains(nike + " 精力不足！双修需要") && flag < 2) {
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), flag == 0 ? "使用 小精力瓶" : "使用 大精力瓶", "双修"));
-				} else if (content.contains(nike + " 您背包里并没有小精力瓶呢")) {
-					flagMap.put(groupId, 1);
-				} else if (content.contains(nike + " 您背包里并没有大精力瓶呢")) {
-					flagMap.put(groupId, 2);
-				} else if (content.contains(nike + " 您的宠物宝宝心情实在是太差了")) {
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "玩耍"));
-				} else if (content.contains(nike + " 您还没有鱼竿呢")) {
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "购买鱼竿 稀有鱼竿", "钓鱼"));
-				} else if (content.contains(nike + " 您的稀有鱼饵已经用完啦")) {
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "购买鱼饵 稀有鱼饵 100", "钓鱼"));
-				} else if (content.contains(nike + " 运气爆棚啦")) {
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "假装看不见", "钓鱼"));
-				} else if (content.contains(nike + " 吼吼，只见鱼儿")) {
-					executor.execute(() -> client.sendMessageToGroup(message.getGroupId(), "钓鱼"));
-				} else if (content.contains("分钟") || content.contains("报时")) {
-					Wait wait = waitMap.get(groupId);
-					ArrayList<String[]> msgs = msgsMap.get(groupId);
-					if (wait != null && msgs != null) {
-						for (int i = 0; i < msgs.size(); i++) {
-							int time = getTime(content, msgs.get(i)[1]);
-							if (31 != time) {
-								wait.setTagTime(msgs.get(i)[0], time * 60 * 1000);
-							}
+			if (robot == userId) {
+				final Integer flag = flagMap.get(chatId);
+				String nike = nikeMap.get(chatId);
+				Wait wait = waitMap.get(chatId);
+				ArrayList<String[]> msgs = msgsMap.get(chatId);
+				if (wait != null && msgs != null) {
+					for (int i = 0; i < msgs.size(); i++) {
+						int time = getTime(content, msgs.get(i)[1]);
+						if (1800 != time) {
+							wait.setTagTime(msgs.get(i)[0], time * 1000);
 						}
 					}
 				}
 			}
 		}
 
-		@Override
-		public void onDiscussMessage(DiscussMessage message) {}
+		private void send(ISendMessage send, String msg, long chatId) {
+			executor.execute(() -> send.sendMessage(msg, chatId));
+		}
 
 		@Override
 		public void close() throws IOException {
@@ -211,7 +190,7 @@ public class Robot {
 				while (isLive) {
 					try {
 						long nowTime = System.currentTimeMillis();
-						if (nowTime - lastTime > 70L * 60 * 1000) {
+						if (nowTime - lastTime > 120L * 60 * 1000) {
 							stop();
 							return;
 						}
@@ -249,15 +228,19 @@ public class Robot {
 		}
 	}
 
+	interface ISendMessage {
+		void sendMessage(String msg, long chatId);
+	}
+
 	private static int getTime(String txt, String regex) {
 		try {
 			Pattern compile = Pattern.compile(regex);
 			Matcher matcher = compile.matcher(txt);
 			if (matcher.find()) {
 				int time = Integer.parseInt(matcher.group(1));
-				return Math.min(time, 31);
+				return Math.min(time, 1800);
 			}
 		} catch (Exception e) {}
-		return 31;
+		return 1800;
 	}
 }
