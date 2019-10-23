cfd695747063dd8d2833a394a6dfc8ebdc799e8a
qiwei <qiweiwuyu@gmail.com>
2019-10-23 17:31:44 +0800

    添加mybatis自动生成配置

--- ignore.bat
+++ ignore.bat
@@ -1,2 +1,3 @@
+git update-index --assume-unchanged mybatis\generatorConfig.xml
 git update-index --assume-unchanged src\main\resources\jdbc.properties
 git update-index --assume-unchanged src\main\java\com\qwwuyu\server\configs\SecretConfig.java
--- show.bat
+++ show.bat
@@ -1,2 +1,3 @@
+git update-index --no-assume-unchanged mybatis\generatorConfig.xml
 git update-index --no-assume-unchanged src\main\resources\jdbc.properties
 git update-index --no-assume-unchanged src\main\java\com\qwwuyu\server\configs\SecretConfig.java
