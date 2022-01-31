9b94699f8c6977bf8836001865660f28abf471f5
qwwuyu <qiweiwuyu@gmail.com>
2022-01-31 11:37:39 +0800

    Update J2EEUtil.kt

--- lib/src/main/kotlin/com/qwwuyu/lib/utils/J2EEUtil.kt
+++ lib/src/main/kotlin/com/qwwuyu/lib/utils/J2EEUtil.kt
@@ -5,25 +5,34 @@ import javax.servlet.http.HttpServletRequest
 object J2EEUtil {
     /** 获取真实ip  */
     fun getRealAddress(request: HttpServletRequest): String {
-        val headers = listOf(
-            "x-forwarded-for", "X-Real-IP", "Proxy-Client-IP", "WL-Proxy-Client-IP",
-            "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"
-        )
-        headers.forEach {
-            val ip = request.getHeader(it)
-            if (!ip.isNullOrEmpty() && !ip.equals("unknown", ignoreCase = true)
-                && !ip.startsWith("10.") && !ip.startsWith("192.168") && !ip.startsWith("172.")
-            ) {
-                return ip
-            }
+        val remoteAddr = request.remoteAddr
+        if ("127.0.0.1" != remoteAddr && "0:0:0:0:0:0:0:1" != remoteAddr) {
+            return remoteAddr
         }
-        return request.remoteAddr
+        val realIp = request.getHeader("X-Real-IP")
+        var ip = realIp
+        if (!ip.isNullOrEmpty() && !ip.equals("unknown", ignoreCase = true) && !ip.equals("0:0:0:0:0:0:0:1")
+            && !ip.startsWith("10.") && !ip.startsWith("192.168") && !ip.startsWith("172.")
+        ) {
+            return ip
+        }
+        ip = request.getHeader("x-forwarded-for")
+        if (ip.contains(",")) {
+            val ips = ip.split(",")
+            ip = ips[ips.size - 2]
+        }
+        if (!ip.isNullOrEmpty() && !ip.equals("unknown", ignoreCase = true) && !ip.equals("0:0:0:0:0:0:0:1")
+            && !ip.startsWith("10.") && !ip.startsWith("192.168") && !ip.startsWith("172.")
+        ) {
+            return ip
+        }
+        return realIp
     }
 
     /** 获取日志ip  */
     fun getLogAddress(request: HttpServletRequest): String {
         var log = " remoteAddr=${request.remoteAddr}"
-        val headers = listOf("x-forwarded-for", "X-Real-IP", "Proxy-Client-IP", "WL-Proxy-Client-IP")
+        val headers = listOf("X-Real-IP", "x-forwarded-for", "Proxy-Client-IP", "WL-Proxy-Client-IP")
         headers.forEach {
             val ip = request.getHeader(it)
             if (!ip.isNullOrEmpty() && !ip.equals("unknown", ignoreCase = true)) {
